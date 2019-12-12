6c4447f17de24896eede4aa65195fbf227af760a
burhanwakhid <burhanudinwakhid@student.uns.ac.id>
2019-12-12 11:35:18 +0700

    add splashscreen and change the primary color

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:tech_task/src/screens/list_ingredient.dart';
+import 'package:tech_task/src/screens/views/list_ingredient.dart';
 import 'package:tech_task/src/splashscreen.dart';
 
 class App extends StatelessWidget {
   final Map<String, WidgetBuilder> routes = {
     '/splashscreen': (context) => SplashScreen(),
-    '/ingredient': (context) => IngredientPage(),
+    '/home': (context) => IngredientPage(),
   };
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TechTask',
       theme: ThemeData(
-        primaryColor: const Color(0xFF01579B),
+        primaryColor: const Color(0xFFFF6D00),
         accentColor: Colors.red,
       ),
       debugShowCheckedModeBanner: false,
       routes: routes,
-      home: IngredientPage(),
+      home: SplashScreen(),
     );
   }
 }
--- lib/src/splashscreen.dart
+++ lib/src/splashscreen.dart
@@ -1,14 +1,59 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+  var _visible = true;
+
+  AnimationController animationController;
+  Animation<double> animation;
+
+  startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return new Timer(_duration, () => Navigator.of(context).pushReplacementNamed('/home'));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = new AnimationController(
+        vsync: this, duration: new Duration(seconds: 2));
+    animation =
+        new CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+
+    animation.addListener(() => this.setState(() {}));
+    animationController.forward();
+
+    setState(() {
+      _visible = !_visible;
+    });
+    startTime();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('LOAD SMILE', style: TextStyle(fontSize: animation.value * 24, color: Colors.white, fontWeight: FontWeight.w500),)
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
